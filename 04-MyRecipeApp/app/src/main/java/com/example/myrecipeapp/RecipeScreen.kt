f2709fb658d77b5129893bfc9d2d457f1a5e7b24
Esteban Cruz <cruzcr6@gmail.com>
2024-06-12 17:19:35 -0600

    Updating Project 04

--- 04-MyRecipeApp/app/src/main/java/com/example/myrecipeapp/RecipeScreen.kt
+++ 04-MyRecipeApp/app/src/main/java/com/example/myrecipeapp/RecipeScreen.kt
@@ -2,6 +2,7 @@ package com.example.myrecipeapp
 
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
@@ -24,7 +25,8 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 
 @Composable
-fun RecipeScreen(modifier: Modifier = Modifier){
+fun RecipeScreen(modifier: Modifier = Modifier,
+                 navigateToDetail: (Category) -> Unit){
     val recipeViewModel: MainViewModel = viewModel()
     val viewState by recipeViewModel.categoriesState
 
@@ -40,28 +42,31 @@ fun RecipeScreen(modifier: Modifier = Modifier){
                 Log.d(javaClass.simpleName, "RecipeScreen: ${viewState.error}")
             }
             else -> {
-                CategoryScreen(categories = viewState.list)
+                CategoryScreen(categories = viewState.list, navigateToDetail)
             }
         }
     }
 }
 
 @Composable
-fun CategoryScreen(categories: List<Category>) {
+fun CategoryScreen(categories: List<Category>,
+                   navigateToDetail: (Category) -> Unit) {
     LazyVerticalGrid(GridCells.Fixed(2), modifier = Modifier.fillMaxSize() ){
         items(categories) {
             category -> 
-                CategoryItem(category = category)
+                CategoryItem(category = category, navigateToDetail )
         }
     }
 }
 
 // How each item look like
 @Composable
-fun CategoryItem(category: Category){
+fun CategoryItem(category: Category,
+                 navigateToDetail: (Category) -> Unit ){
     Column(modifier = Modifier
         .padding(8.dp)
-        .fillMaxSize(),
+        .fillMaxSize()
+        .clickable { navigateToDetail(category) },
             horizontalAlignment = Alignment.CenterHorizontally) {
         Image(
             // Loading an image from internet asynchronously
